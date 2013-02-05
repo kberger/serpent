636d9b65c4e358e2a6194a431d76da173e593bcf
Dan <dan.grau@gmail.com>
2013-02-05 01:13:27 -0500

    IP and FP, and encrypt functions, along with basic main(). Untested.

--- src/Serpent.java
+++ src/Serpent.java
@@ -3,6 +3,7 @@ import edu.rit.util.Packing;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
+import java.lang.Integer;
 
 public class Serpent implements BlockCipher {
 
@@ -93,36 +94,90 @@ public class Serpent implements BlockCipher {
      * @param  text  Plaintext (on input), ciphertext (on output).
      */
     public void encrypt(byte[] text) {
-
+        text = initPermutation(text);
+        byte[] roundKey = new byte[16];
+        //32 rounds
+        for(int i = 0; i < 32; i++){
+            roundKey = getRoundKey(i);
+            for(int n = 0; n < 16; n++){
+                text[n] = text[n] ^ roundKey[n];
+            }
+            text = sBox(text, i);
+            text = linearTransform(text);
+        }
+        roundKey = getRoundKey(32);
+        for(int n = 0; n < 16; n++){
+            text[n] = text[n] ^ roundKey[n];
+        }
+        text = finalPermutation(text);
     }
 
 
-    private void initPermutation(byte[] data) {
+    private byte[] initPermutation(byte[] data) {
+        byte[] output = new byte[16];
+        for (int i = 0;  i < 128; i++) {
+            int bit = (input[(ipTable[i] & 0x7F) / 8] >>> ((ipTable[i] & 0x7F) % 8)) & 0x01;
+            if ((bit & 0x01) == 1)
+                output[i / 8] |= 1 << (i % 32);
+            else
+                output[i / 8] &= ~(1 << (i % 32));
+        }
+        return output; 
         
     }
 
-    private void finalPermutation(byte[] data) {
-
+    private byte[] finalPermutation(byte[] data) {
+        byte[] output = new byte[16];
+        for (int i = 0;  i < 128; i++) {
+            int bit = (input[(fpTable[i] & 0x7F) / 8] >>> ((fpTable[i] & 0x7F) % 8)) & 0x01;
+            if ((bit & 0x01) == 1)
+                output[i / 8] |= 1 << (i % 32);
+            else
+                output[i / 8] &= ~(1 << (i % 32));
+        }
+        return output; 
     }
 
-    private static long[] s0 = new long[]
+    private static byte[] s0 = new byte[]
         {3,8,15,1,10,6,5,11,14,13,4,2,7,0,9,12};
-    private static long[] s1 = new long[]
+    private static byte[] s1 = new byte[]
         {15,12,2,7,9,0,5,10,1,11,14,8,6,13,3,4};
-    private static long[] s2 = new long[]
+    private static byte[] s2 = new byte[]
         {8,6,7,9,3,12,10,15,13,1,14,4,0,11,5,2};
-    private static long[] s3 = new long[]
+    private static byte[] s3 = new byte[]
         {0,15,11,8,12,9,6,3,13,1,2,4,10,7,5,14};
-    private static long[] s4 = new long[]
+    private static byte[] s4 = new byte[]
         {1,15,8,3,12,0,11,6,2,5,4,10,9,14,7,13};
-    private static long[] s5 = new long[]
+    private static byte[] s5 = new byte[]
         {15,5,2,11,4,10,9,12,0,3,14,8,13,6,7,1};
-    private static long[] s6 = new long[]
+    private static byte[] s6 = new byte[]
         {7,2,12,5,8,4,6,11,14,9,1,15,13,3,10,0};
-    private static long[] s7 = new long[]
+    private static byte[] s7 = new byte[]
         {1,13,15,0,14,8,2,11,7,4,12,10,9,3,5,6};
-    private static long[][] sBoxes = new long[][]
+    private static byte[][] sBoxes = new byte[][]
         {s0,s1,s2,s3,s4,s5,s6,s7};
+
+    private static byte[] ipTable = new byte[] {
+         0, 32, 64,  96,  1, 33, 65,  97,  2, 34, 66,  98,  3, 35, 67,  99,
+         4, 36, 68, 100,  5, 37, 69, 101,  6, 38, 70, 102,  7, 39, 71, 103,
+         8, 40, 72, 104,  9, 41, 73, 105, 10, 42, 74, 106, 11, 43, 75, 107,
+        12, 44, 76, 108, 13, 45, 77, 109, 14, 46, 78, 110, 15, 47, 79, 111,
+        16, 48, 80, 112, 17, 49, 81, 113, 18, 50, 82, 114, 19, 51, 83, 115,
+        20, 52, 84, 116, 21, 53, 85, 117, 22, 54, 86, 118, 23, 55, 87, 119,
+        24, 56, 88, 120, 25, 57, 89, 121, 26, 58, 90, 122, 27, 59, 91, 123,
+        28, 60, 92, 124, 29, 61, 93, 125, 30, 62, 94, 126, 31, 63, 95, 127
+    };
+
+    private static byte[] fpTable = new byte[] {
+         0,  4,  8, 12, 16, 20, 24, 28, 32,  36,  40,  44,  48,  52,  56,  60,
+        64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124,
+         1,  5,  9, 13, 17, 21, 25, 29, 33,  37,  41,  45,  49,  53,  57,  61,
+        65, 69, 73, 77, 81, 85, 89, 93, 97, 101, 105, 109, 113, 117, 121, 125,
+         2,  6, 10, 14, 18, 22, 26, 30, 34,  38,  42,  46,  50,  54,  58,  62,
+        66, 70, 74, 78, 82, 86, 90, 94, 98, 102, 106, 110, 114, 118, 122, 126,
+         3,  7, 11, 15, 19, 23, 27, 31, 35,  39,  43,  47,  51,  55,  59,  63,
+        67, 71, 75, 79, 83, 87, 91, 95, 99, 103, 107, 111, 115, 119, 123, 127
+    };
 
     /**
      * Perform S-Box manipulation to the given byte array of <TT>blocksize()</TT> length.
@@ -131,14 +186,14 @@ public class Serpent implements BlockCipher {
      * @param round Number of the current round, used to determine which S-Box to use.
      */
     private byte[] sBox(byte[] data, int round) {
-        long[] toUse = sBoxes[round%8];
+        byte[] toUse = sBoxes[round%8];
         byte[] output = new byte[blockSize()];
         for( int i = 0; i < blockSize(); i++ ) {
             //Break signed-ness
             int curr = data[i]&0xFF;
             byte low4 = (byte)(curr>>>4);
             byte high4 = (byte)(curr&0x0F);
-            output[i] = (byte)((toUse[low4]<<4) ^ (toUse[high4]));
+            output[i] = (toUse[low4]<<4) ^ (toUse[high4]);
         }
         return output;
     }
@@ -207,7 +262,27 @@ public class Serpent implements BlockCipher {
         return sBox(k,(3-round)%8);
     }
 
+    /**
+     * Main function, sets an all-zero-byte key, performs N encryptions of an all-zero-byte plaintext block
+     * N specified in args
+     */
     public static void main( String[] args ) {
+        Serpent serpent = new Serpent();
+        byte[] test_in = new byte[] {
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+        };
+        byte[] test_key = new byte[] {
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+        };
+        int iters = Integer.parseInt(args[0]);
+        for(int n = 0; n < iters; n++){
+            serpent.setKey(test_key);
+            serpent.encrypt(test_in);
+        }
         //sBoxTest();
         //setKeyTest();
     }
