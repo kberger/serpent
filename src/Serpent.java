ff1705de472dc3f19bbff2bba7e79390101a1278
Karl Berger <krab.berger@gmail.com>
2013-02-06 12:16:35 -0500

    IP testing.

--- src/Serpent.java
+++ src/Serpent.java
@@ -398,10 +398,10 @@ public class Serpent implements BlockCipher {
     private static void IPTest() {
         Serpent tSerp = new Serpent();
         byte[] test = new byte[]{
-            0x00,0x01,0x02,0x03,
-            0x04,0x05,0x06,0x07,
-            0x08,0x09,0x0a,0x0b,
-            0x0c,0x0d,0x0e,0x0f
+            (byte)0x12,(byte)0x34,(byte)0x56,(byte)0x78,
+            (byte)0x9a,(byte)0xbc,(byte)0xde,(byte)0xff,
+            (byte)0x12,(byte)0x34,(byte)0x56,(byte)0x78,
+            (byte)0x9a,(byte)0xbc,(byte)0xde,(byte)0xff
         };
         System.out.println("IP Test1: "+Hex.toString(test));
         System.out.println("Out Test1: "+Hex.toString(tSerp.initPermutation(test)));
